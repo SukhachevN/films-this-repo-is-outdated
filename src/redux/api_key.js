611af6da3679f967721b96ef73540ffab8df9c11
SukhachevN <43090884+SukhachevN@users.noreply.github.com>
2021-09-12 18:48:45 +0300

    reset (#2)

--- src/redux/api_key.js
+++
@@ -1,1 +0,0 @@
-export const api_key = 'f96bc754e888b05f53dd6db062184947'
--- src/screens/NotFoundScreen.js
+++
@@ -1,5 +0,0 @@
-import notFound from '../img/not-found.png'
-function NotFoundScreen(){
-    return <img src={notFound} alt='not found' className='NotFoundScreen'/>
-}
-export {NotFoundScreen}
